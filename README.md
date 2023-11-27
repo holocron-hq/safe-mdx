360df8e98b580d42b7dfbd2f43f75825e13b9fe0
daertommy <morsesdevstuff@gmail.com>
2023-11-27 17:35:10 +0100

    init

---
+++ README.md
@@ -0,0 +1,86 @@
+<div align='center'>
+    <br/>
+    <br/>
+    <br/>
+    <h3>safe-mdx</h3>
+    <p>Render MDX in React without eval</p>
+    <br/>
+    <br/>
+</div>
+
+## Why
+
+The default MDX renderer uses `eval` to render MDX components. This is a security risk and it's not allowed in some environments like Cloudflare Workers.
+
+Some use cases for this package are:
+
+-   Render MDX in Cloudflare Workers and Vercel Edge
+-   Safely render dynamically generated MDX code, like inside a ChatGPT like interface
+-   Render user generated MDX, like in a multi-tenant SaaS app
+
+<br>
+
+## Install
+
+```
+npm i safe-mdx
+```
+
+## Usage
+
+```tsx
+import { SafeMdxRenderer } from 'safe-mdx'
+
+const code = `
+# Hello world
+
+This is a paragraph
+
+<Heading>Custom component</Heading>
+`
+
+export function Page() {
+    return (
+        <MdxRenderer
+            code={code}
+            components={{
+                // You can pass your own components here
+                Heading({ children }) {
+                    return <h1>{children}</h1>
+                },
+            }}
+        />
+    )
+}
+```
+
+## Change default MDX parser
+
+If you want to use custom MDX plugins, you can pass your own MDX processed ast.
+
+By default `safe-mdx` already has support for
+
+-   frontmatter
+-   gfm
+
+```tsx
+import { SafeMdxRenderer } from 'safe-mdx'
+import { remark } from 'remark'
+import remarkMdx from 'remark-mdx'
+
+const code = `
+# Hello world
+
+This is a paragraph
+
+<Heading>Custom component</Heading>
+`
+
+const parser = remark().use(remarkMdx)
+
+const mdast = parser.parse(code)
+
+export function Page() {
+    return <MdxRenderer code={code} mdast={mdast} />
+}
+```
---
+++ src/safe-mdx.tsx
@@ -0,0 +1,481 @@
+import { htmlToJsx } from 'html-to-jsx-transform'
+import { Node, Parent } from 'mdast'
+import remarkFrontmatter from 'remark-frontmatter'
+
+import { Root, RootContent } from 'mdast'
+import { MdxJsxFlowElement, MdxJsxTextElement } from 'mdast-util-mdx-jsx'
+import { remark } from 'remark'
+import remarkGfm from 'remark-gfm'
+import remarkMdx from 'remark-mdx'
+
+import { Fragment, ReactNode } from 'react'
+
+type MyRootContent = RootContent | Root
+
+const mdxParser = remark()
+    .use(remarkFrontmatter, ['yaml', 'toml'])
+    .use(remarkGfm)
+    .use(remarkMdx) as any
+
+export function SafeMdxRenderer({
+    components,
+    code = '',
+    mdast = null as any,
+}) {
+    const visitor = new MdastToJsx({ code, mdast, components })
+    const result = visitor.run()
+    return result
+}
+
+const nativeTags = [
+    'blockquote',
+    'strong',
+    'em',
+    'del',
+    'br',
+    'hr',
+    'a',
+    'b',
+    'br',
+    'button',
+    'div',
+    'form',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'head',
+    'iframe',
+    'img',
+    'input',
+    'label',
+    'li',
+    'link',
+    'ol',
+    'p',
+    'path',
+    'picture',
+    'script',
+    'section',
+    'source',
+    'span',
+    'sub',
+    'sup',
+    'svg',
+    'table',
+    'tbody',
+    'td',
+    'th',
+    'thead',
+    'tr',
+    'ul',
+    'video',
+    'code',
+    'pre',
+] as const
+
+type ComponentsMap = { [k in (typeof nativeTags)[number]]: any }
+
+class MdastToJsx {
+    mdast: MyRootContent
+    str: string
+    jsxStr: string = ''
+    c: ComponentsMap
+    errors: string[] = []
+    constructor({ code = '', mdast, components = {} as ComponentsMap }) {
+        this.str = code
+        this.mdast = mdast || mdxParser.parse(code)
+        // TODO add tags and their allowed import sources
+        this.c = {
+            ...Object.fromEntries(
+                nativeTags.map((tag) => {
+                    return [tag, tag]
+                }),
+            ),
+            ...components,
+        }
+    }
+    mapMdastChildren(node: any) {
+        const res = node.children?.flatMap((child) =>
+            this.mdastTransformer(child),
+        )
+        if (Array.isArray(res)) {
+            if (res.length === 1) {
+                return res[0]
+            } else {
+                return res.map((x, i) => <Fragment key={i}>{x}</Fragment>)
+            }
+        }
+        return res || null
+    }
+    mapJsxChildren(node: any) {
+        const res = node.children?.flatMap((child, i) =>
+            this.jsxTransformer(child),
+        )
+        if (Array.isArray(res)) {
+            if (res.length === 1) {
+                return res[0]
+            } else {
+                return res.map((x, i) => <Fragment key={i}>{x}</Fragment>)
+            }
+        }
+        return res || null
+    }
+    jsxTransformer(node: MyRootContent): ReactNode {
+        if (!node) {
+            return []
+        }
+
+        switch (node.type) {
+            case 'mdxJsxTextElement':
+            case 'mdxJsxFlowElement': {
+                if (!node.name) {
+                    return []
+                }
+
+                const Component = accessWithDot(this.c, node.name)
+
+                if (!Component) {
+                    this.errors.push(`Unsupported jsx tag ${node.name}`)
+                    return null
+                }
+
+                let attrsList = this.getJsxAttrs(node)
+
+                let attrs = Object.fromEntries(attrsList)
+                return (
+                    <Component {...attrs}>
+                        {this.mapJsxChildren(node)}
+                    </Component>
+                )
+            }
+            default: {
+                return this.mdastTransformer(node)
+            }
+        }
+    }
+
+    run() {
+        const res = this.mdastTransformer(this.mdast) as ReactNode
+        if (Array.isArray(res) && res.length === 1) {
+            return res[0]
+        }
+        return res
+    }
+
+    mdastTransformer(node: MyRootContent): ReactNode {
+        if (!node) {
+            return []
+        }
+
+        switch (node.type) {
+            case 'mdxjsEsm': {
+                const start = node.position?.start?.offset
+                const end = node.position?.end?.offset
+                let text = this.str.slice(start, end)
+                // console.log('esm', node)
+                const tree = node.data?.estree
+
+                return []
+            }
+            case 'mdxJsxTextElement':
+            case 'mdxJsxFlowElement': {
+                const start = node.position?.start?.offset
+                const end = node.position?.end?.offset
+                const text = this.str.slice(start, end)
+                try {
+                    this.jsxStr = text
+                    const result = this.jsxTransformer(node)
+                    if (Array.isArray(result)) {
+                        console.log(`Unexpected array result`)
+                    } else if (result) {
+                        return result
+                    }
+                } finally {
+                    this.jsxStr = ''
+                }
+                return []
+            }
+
+            case 'mdxFlowExpression':
+            case 'mdxTextExpression': {
+                if (!node.value) {
+                    return []
+                }
+                return []
+            }
+            case 'yaml': {
+                if (!node.value) {
+                    return []
+                }
+                return []
+            }
+            case 'heading': {
+                const level = node.depth
+
+                const Tag: any = `h${level}`
+                return <Tag>{this.mapMdastChildren(node)}</Tag>
+            }
+            case 'paragraph': {
+                return <this.c.p>{this.mapMdastChildren(node)}</this.c.p>
+            }
+            case 'blockquote': {
+                return (
+                    <this.c.blockquote>
+                        {this.mapMdastChildren(node)}
+                    </this.c.blockquote>
+                )
+            }
+            case 'thematicBreak': {
+                return <this.c.hr />
+            }
+            case 'code': {
+                if (!node.value) {
+                    return []
+                }
+                const language = node.lang || ''
+                const code = node.value
+                return (
+                    <this.c.pre>
+                        <this.c.code className={`language-${language}`}>
+                            {code}
+                        </this.c.code>
+                    </this.c.pre>
+                )
+            }
+
+            case 'list': {
+                if (node.ordered) {
+                    return (
+                        <this.c.ol start={node.start!}>
+                            {this.mapMdastChildren(node)}
+                        </this.c.ol>
+                    )
+                }
+                return <this.c.ul>{this.mapMdastChildren(node)}</this.c.ul>
+            }
+            case 'listItem': {
+                // https://github.com/syntax-tree/mdast-util-gfm-task-list-item#syntax-tree
+                if (node?.checked != null) {
+                    return (
+                        <this.c.li data-checked={node.checked}>
+                            {this.mapMdastChildren(node)}
+                        </this.c.li>
+                    )
+                }
+                return <this.c.li>{this.mapMdastChildren(node)}</this.c.li>
+            }
+            case 'text': {
+                if (!node.value) {
+                    return []
+                }
+                return node.value
+            }
+            case 'image': {
+                const src = node.url || ''
+                const alt = node.alt || ''
+                const title = node.title || ''
+                return <this.c.img src={src} alt={alt} title={title} />
+            }
+            case 'link': {
+                const href = node.url || ''
+                const title = node.title || ''
+                return (
+                    <this.c.a {...{ href, title }}>
+                        {this.mapMdastChildren(node)}
+                    </this.c.a>
+                )
+            }
+            case 'strong': {
+                return (
+                    <this.c.strong>{this.mapMdastChildren(node)}</this.c.strong>
+                )
+            }
+            case 'emphasis': {
+                return <this.c.em>{this.mapMdastChildren(node)}</this.c.em>
+            }
+            case 'delete': {
+                return <this.c.del>{this.mapMdastChildren(node)}</this.c.del>
+            }
+            case 'inlineCode': {
+                if (!node.value) {
+                    return []
+                }
+                return <this.c.code>{node.value}</this.c.code>
+            }
+            case 'break': {
+                return <this.c.br />
+            }
+            case 'root': {
+                return (
+                    <this.c.div className=''>
+                        {this.mapMdastChildren(node)}
+                    </this.c.div>
+                )
+            }
+            case 'table': {
+                const align = node.align
+                return (
+                    <this.c.table>{this.mapMdastChildren(node)}</this.c.table>
+                )
+            }
+            case 'tableRow': {
+                return (
+                    <this.c.tr className=''>
+                        {this.mapMdastChildren(node)}
+                    </this.c.tr>
+                )
+            }
+            case 'tableCell': {
+                let content = this.mapMdastChildren(node)
+
+                return <this.c.td className=''>{content}</this.c.td>
+            }
+            case 'definition': {
+                return []
+            }
+            case 'footnoteReference': {
+                return []
+            }
+
+            case 'footnoteDefinition': {
+                return []
+            }
+            case 'html': {
+                // console.log('html', node)
+
+                const start = node.position?.start?.offset
+                const end = node.position?.end?.offset
+                const text = this.str.slice(start, end)
+                if (!text) {
+                    return []
+                }
+
+                const jsx = htmlToJsx(text)
+                try {
+                    this.jsxStr = jsx
+                    const result = this.jsxTransformer(node)
+                    if (Array.isArray(result)) {
+                        console.log(`Unexpected array result`)
+                    } else if (result) {
+                        return result
+                    }
+                } finally {
+                    this.jsxStr = ''
+                }
+
+                return []
+            }
+            case 'imageReference': {
+                return []
+            }
+
+            default: {
+                mdastBfs(node, (node) => {
+                    delete node.position
+                })
+
+                throw new Error(
+                    `cannot convert node` + JSON.stringify(node, null, 2),
+                )
+
+                return []
+            }
+        }
+    }
+    getJsxAttrs(node: MdxJsxFlowElement | MdxJsxTextElement) {
+        let attrsList = node.attributes
+            .map((attr) => {
+                // TODO what is mdxJsxExpressionAttribute
+                if (attr.type === 'mdxJsxExpressionAttribute') {
+                    throw new Error(
+                        `mdxJsxExpressionAttribute is not supported: ${attr.value}`,
+                    )
+                }
+                if (attr.type !== 'mdxJsxAttribute') {
+                    throw new Error(
+                        `non mdxJsxAttribute is not supported: ${attr}`,
+                    )
+                }
+
+                const v = attr.value
+                if (typeof v === 'string' || typeof v === 'number') {
+                    return [attr.name, v]
+                }
+                if (v === null) {
+                    return [attr.name, true]
+                }
+                if (v?.type === 'mdxJsxAttributeValueExpression') {
+                    // logger.json({value})
+                    // if it's a number, just return it
+                    if (v.value === 'true') {
+                        return [attr.name, true]
+                    }
+                    if (v.value === 'false') {
+                        return [attr.name, false]
+                    }
+                    if (v.value === 'null') {
+                        return [attr.name, null]
+                    }
+                    if (v.value === 'undefined') {
+                        return [attr.name, undefined]
+                    }
+                    if (
+                        // TODO add a way to parse ' and `
+                        ['"'].some(
+                            (q) => v.value.startsWith(q) && v.value.endsWith(q),
+                        )
+                    ) {
+                        return [attr.name, JSON.parse(v.value)]
+                    }
+
+                    const number = Number(v.value)
+                    if (!isNaN(number)) {
+                        return [attr.name, number]
+                    }
+
+                    this.errors.push(
+                        `Expressions in jsx props are not supported (${attr.name}={${v.value}})`,
+                    )
+
+                    // return [attr.name, `{${v.value}}`]
+                } else {
+                    console.log('unhandled attr', { attr }, attr.type)
+                }
+
+                return
+            })
+            .filter(isTruthy) as [string, any][]
+        return attrsList
+    }
+}
+
+function isTruthy<T>(val: T | undefined | null | false): val is T {
+    return Boolean(val)
+}
+
+function accessWithDot(obj, path: string) {
+    return path
+        .split('.')
+        .map((x) => x.trim())
+        .filter(Boolean)
+        .reduce((o, i) => o[i], obj)
+}
+
+function mdastBfs(node: Parent | Node, cb?: (node: Node | Parent) => any) {
+    const queue = [node]
+    const result: any[] = []
+    while (queue.length) {
+        const node = queue.shift()
+        let r = cb && node ? cb(node) : node
+        if (Array.isArray(r)) {
+            queue.push(...r)
+        } else if (r) {
+            result.push(r)
+        }
+        if (node && 'children' in node && node.children) {
+            queue.push(...(node.children as any))
+        }
+    }
+    return result
+}
