39aa2dde3a3ace3f7fac0e350bb44d9f99ba72a3
remorses <beats.by.morse@gmail.com>
2025-08-03 19:23:08 +0200

    faster demo

--- demo/main.tsx
+++ demo/main.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
 
+const [reactMod, reactDomMod, jsxRuntimeMod] = await Promise.all([
+    import('./import-map/react'),
+    import('./import-map/react-dom'),
+    import('./import-map/react/jsx-runtime'),
+])
+
 const mapScript = document.createElement('script')
 mapScript.type = 'importmap'
 mapScript.textContent = JSON.stringify(
     {
         imports: {
-            react: (await import('./import-map/react')).url,
-            'react-dom': (await import('./import-map/react-dom')).url,
-            'react/jsx-runtime': (
-                await import('./import-map/react/jsx-runtime')
-            ).url,
-
+            react: reactMod.url,
+            'react-dom': reactDomMod.url,
+            'react/jsx-runtime': jsxRuntimeMod.url,
             'framer-motion': 'https://esm.sh/framer-motion?external=react',
             '@motionone/dom': 'https://esm.sh/@motionone/dom?external=react',
             framer: 'https://esm.sh/unframer@latest/esm/framer.js?external=react',
