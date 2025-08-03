585a58392f82a164326bf63e6c218e746293deb5
remorses <beats.by.morse@gmail.com>
2025-08-03 19:35:14 +0200

    fix build

--- demo/main.tsx
+++ demo/main.tsx
@@ -2,28 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
 
-const [reactMod, reactDomMod, jsxRuntimeMod] = await Promise.all([
-    import('./import-map/react'),
-    import('./import-map/react-dom'),
-    import('./import-map/react/jsx-runtime'),
-])
+async function setupImportMap() {
+    const [reactMod, reactDomMod, jsxRuntimeMod] = await Promise.all([
+        import('./import-map/react'),
+        import('./import-map/react-dom'),
+        import('./import-map/react/jsx-runtime'),
+    ])
 
-const mapScript = document.createElement('script')
-mapScript.type = 'importmap'
-mapScript.textContent = JSON.stringify(
-    {
-        imports: {
-            react: reactMod.url,
-            'react-dom': reactDomMod.url,
-            'react/jsx-runtime': jsxRuntimeMod.url,
-            'framer-motion': 'https://esm.sh/framer-motion?external=react',
-            '@motionone/dom': 'https://esm.sh/@motionone/dom?external=react',
-            framer: 'https://esm.sh/unframer@latest/esm/framer.js?external=react',
+    const mapScript = document.createElement('script')
+    mapScript.type = 'importmap'
+    mapScript.textContent = JSON.stringify(
+        {
+            imports: {
+                react: reactMod.url,
+                'react-dom': reactDomMod.url,
+                'react/jsx-runtime': jsxRuntimeMod.url,
+                'framer-motion': 'https://esm.sh/framer-motion?external=react',
+                '@motionone/dom':
+                    'https://esm.sh/@motionone/dom?external=react',
+                framer: 'https://esm.sh/unframer@latest/esm/framer.js?external=react',
+            },
         },
-    },
-    null,
-    2,
-)
-document.head.append(mapScript)
+        null,
+        2,
+    )
+    document.head.append(mapScript)
+}
+setupImportMap()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 import type { Plugin, UserConfig } from 'vite'
 
 export default defineConfig({
-    // resolve: {
-    //     alias: {
-    //         react: 'https://esm.sh/react',
-    //         'react/jsx-runtime': 'https://esm.sh/react/jsx-runtime',
-    //         'react/jsx-dev-runtime': 'https://esm.sh/react/jsx-dev-runtime',
-    //     },
-    // },
-    // build: {
-    //     rollupOptions: {
-    //         external: ['react', 'react/jsx-runtime', 'react/jsx-dev-runtime'],
-    //     },
-    // },
     plugins: [react(), tailwindcss()],
 })
