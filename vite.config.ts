4a6ff3df50e4850f4832953a728b7733710b50db
remorses <beats.by.morse@gmail.com>
2025-08-04 13:23:25 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -102,6 +102,7 @@ export default defineConfig({
                 'react': './demo/import-map/react',
                 'react-dom': './demo/import-map/react-dom',
                 'react/jsx-runtime': './demo/import-map/react/jsx-runtime',
+                'react/jsx-runtime-dev': './demo/import-map/react/jsx-runtime',
                 'framer-motion': 'https://esm.sh/framer-motion?external=react',
                 '@motionone/dom': 'https://esm.sh/@motionone/dom?external=react',
                 'framer': 'https://esm.sh/unframer@latest/esm/framer.js?external=react',
