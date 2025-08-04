732320d927471d28e12c9fa79fa8a5d7f551b53a
remorses <beats.by.morse@gmail.com>
2025-08-04 13:21:21 +0200

    Update main.tsx

--- demo/main.tsx
+++ demo/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { App } from './app'
 import importMap from 'virtual:importmap'
 
-async function setupImportMap() {
+function setupImportMap() {
     const mapScript = document.createElement('script')
     mapScript.type = 'importmap'
     mapScript.textContent = JSON.stringify(importMap, null, 2)
     console.log(mapScript.textContent)
     document.head.append(mapScript)
 }
-await setupImportMap()
+setupImportMap()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
